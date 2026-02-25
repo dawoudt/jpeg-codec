22451a2ddd483bd2fa01860cada3b1cff97aefba
Dawoud Tabboush <dtabboush@gmail.com>
2026-02-26 01:44:19 +1100

    Minor changes
    - fba allocator
    - grab path from command line
    - start parsing bytes

--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,39 @@
 const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
-    _ = init;
-    const allocator = std.heap.page_allocator;
+    const io = init.io;
+    var args_buffer: [2048]u8 = undefined;
+    var fba: std.heap.FixedBufferAllocator = .init(&args_buffer);
+    const alloc = fba.threadSafeAllocator();
+    const args = try init.minimal.args.toSlice(alloc);
 
-    var threaded: std.Io.Threaded = .init(allocator, .{
-        .argv0 = .empty,
-        .environ = .empty,
-    });
-    defer threaded.deinit();
-    const io = threaded.io();
+    var path = std.mem.splitBackwardsAny(u8, args[1], "/");
+    const file_name = path.first();
+    const directory_path = path.rest();
 
-    var buffer: [1024]u8 = undefined;
+    const directory: std.Io.Dir = try .openDirAbsolute(io, directory_path, .{});
+    defer directory.close(io);
 
-    const d: std.Io.Dir = try .openDirAbsolute(io, "/Users/dtabboush/Downloads/", .{});
-    defer d.close(io);
+    var file = try directory.openFile(io, file_name, .{ .mode = .read_only });
+    defer file.close(io);
 
-    var f = try d.openFile(io, "JPEG_example_flower.jpg", .{ .mode = .read_only });
-    defer f.close(io);
-    var fr = f.reader(io, &buffer);
-    var fr_int = &fr.interface;
-
-    const bytes_to_read = 16;
-    var idx: usize = 0;
-    std.debug.print("out: ", .{});
-
-    while (idx < bytes_to_read) : (idx += 1) {
-        const b = try fr_int.takeByte();
-        std.debug.print("{x} ", .{b});
+    var file_buffer: [1024]u8 = undefined;
+    var file_reader = file.reader(io, &file_buffer);
+    var file_reader_intf = &file_reader.interface;
+    while (true) {
+        const byte = try file_reader_intf.takeByte();
+        if (byte == MARKER) {
+            const next_byte = try file_reader_intf.takeByte();
+            if (next_byte == MARKER_CODE_SOI) {
+                std.debug.print("SOI: {x}{x}", .{ byte, next_byte });
+            } else if (next_byte == MARKER_CODE_EOI) {
+                std.debug.print("SOI: {x}{x}", .{ byte, next_byte });
+                return 0;
+            }
+        }
     }
 }
+
+const MARKER = 0xFF;
+const MARKER_CODE_SOI = 0xD8; // start of image
+const MARKER_CODE_EOI = 0xD8; // end of image
